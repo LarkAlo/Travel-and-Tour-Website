06a4ee3abefc7b4ac42f73c9a73cef05f8b9ba1c
Kimberly Cossio <cossio.alo@outlook.com>
2023-06-19 18:24:02 -0600

    added logo icon
    react-icons

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import "./Navbar.css";
+import {SiYourtraveldottv} from 'react-icons/si'
 
 const Navbar = () => {
-  return <section className='navBarSection'>
-    <header className="header flex">
-      <div className="logoDiv">
-        
-      </div>
-    </header>
-  </section>section>
+  return (
+    <section className="navBarSection">
+      <header className="header flex">
+        <div className="logoDiv">
+          <a href="#" className="logo flex">
+            <h1>
+              <SiYourtraveldottv classname="icon" />
+              Travel.
+            </h1>
+          </a>
+        </div>
+      </header>
+    </section>
+  );
 };
 
 export default Navbar;
