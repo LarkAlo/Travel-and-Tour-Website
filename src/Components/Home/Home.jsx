7d711a2e351628a9382912047148cda488547a71
Kimberly Cossio <cossio.alo@outlook.com>
2023-07-12 17:54:08 -0600

    Assests added
    img and videos added to folder src, currently working on home.html and css

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Home.css";
+import video from "../../Assest/video1.mp4";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <div className="overlay"></div>
+      <video src={video} muted autoPlay loop type="video/mp4"></video>
+      <div className="homeContent  container">
+        <div className="textDiv">
+          <span className="smallText">Our Packages</span>
+          <h1 className="homeTitle">Search your Holiday</h1>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
